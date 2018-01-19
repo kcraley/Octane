2c579c0680b835e0b56368d39eb7294ac2d125ba
Keith Craley <kcral21@gmail.com>
2018-01-18 22:09:09 -0500

    Refactoring for es2017

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,8 @@
 // Configure dependencies
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth/auth.json');
-//var commands = require('./commands/*');
-
-import glob from 'glob';
+import Discord from 'discord.io';
+import logger from 'winston';
+import auth from './auth/auth.json';
+import chalk from 'chalk';
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -14,7 +12,7 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 
 // Initialize Discord Bot
-var bot = new Discord.Client({
+let bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
@@ -27,8 +25,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+        let args = message.substring(1).split(' ');
+        let cmd = args[0];
 
         args = args.splice(1);
         switch(cmd) {
