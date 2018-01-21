d594aa1c6036cff4bacdab09b124ab979439d7b1
Keith Craley <kcral21@gmail.com>
2018-01-21 14:52:47 -0500

    Adding module for environment configuration

---
+++ index.js
@@ -0,0 +1,19 @@
+import logger from 'winston';
+import chalk from 'chalk';
+import moment from 'moment';
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+if (process.env.NODE_ENV === 'prod') {
+  logger.info(chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting in production environment...`));
+  require('./dist/');
+} else {
+  logger.info(chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting in development environment...`));
+  require('babel-register');
+  require('./src/bot.js');
+}
